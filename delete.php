fbe006e0db4dd41340890eea41a03a53b3334fe2
Luis Solorzano <luisnic20@gmail.com>
2016-05-27 17:23:17 -0600

    json#13: insersion de json a update, delete

--- delete.php
+++ delete.php
@@ -9,11 +9,21 @@
 try {
     include 'conexion.php';
 
+    $response = ["success" => 0, "error" => 0, "error_msg" => ""];
     $idn = $_POST['id'];
 
     $sql = $con->prepare("DELETE from notas WHERE id =:id");
     $sql->bindParam(':id', $idn);
     $sql->execute();
+
+    if($sql) {
+        $response["success"] = 1;
+        echo json_encode($response);
+    }else{
+        $response["error"] = 4;
+        $response["error_msg"] = "Ocurrio un error al eliminar el dato...";
+        echo json_encode($response);
+    }
     
     $con = null;
 } catch(PDOException $e) {
