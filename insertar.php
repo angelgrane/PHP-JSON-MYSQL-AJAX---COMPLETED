fb5fe83224629f68cf6e628762cea958098ab255
Luis Solorzano <luisnic20@gmail.com>
2016-05-27 16:56:33 -0600

    json#12: insersion de json a insertar.

--- insertar.php
+++ insertar.php
@@ -11,9 +11,20 @@ if (isset($_POST['nota'])) {
     try {
         include 'conexion.php';
 
-            $sql = "INSERT INTO notas (nota) VALUES (:nota)" or die('no inserto');
+        $response = ["success" => 0, "error" => 0, "error_msg" => ""];
+
+            $sql = "INSERT INTO notas (nota) VALUES (:nota)";
             $q = $con->prepare($sql);
             $q->execute(array('nota'=>$nota));
+
+                if($sql) {
+                    $response["success"] = 1;
+                    echo json_encode($response);
+                }else{
+                    $response["error"] = 4;
+                    $response["error_msg"] = "Ocurrio un error al guardar el dato...";
+                    echo json_encode($response);
+                }
         
             $con = null;
     } catch(PDOException $e) {
